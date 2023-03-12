c0e2361ecde86b1b8f6950cc52bce039a95c6a44
fernando <ferox_callejeros@hotmail.com>
2023-03-12 00:29:29 -0300

    creando proyecto

---
+++ src/main/java/com/example/EcommerceGpt/EcommerceGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceGpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcommerceGpt/EcommerceGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceGpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
